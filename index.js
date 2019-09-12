40546905634c6ee68b3aa19171ef84527965a5ea
ChristianGigante <christian.gigante@student.passerellesnumeriques.org>
2019-09-12 09:58:06 +0800

    initial Commit

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 8000;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', function(socket){
+  console.log('a user connected ' + socket.client);
+ 
+});
+
 
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
